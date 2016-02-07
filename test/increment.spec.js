b5c82cad1ed06fc0bf0fb94ca0e4412b8d007135
widmogrod <widmogrod@gmail.com>
2016-02-07 23:49:42 +0100

    test CRDT axioms for increment

---
+++ test/increment.spec.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const Increment = require('../src/increment');
+const f = require('../src/functions');
+const assert = require('assert')
+
+describe('Increment', () => {
+  describe('#url()', () => {
+    let a,b,c;
+
+    beforeEach(() => {
+       a = new Increment(1);
+       b = new Increment(3);
+       c = new Increment(7);
+    });
+
+    it('should obey CRDT axioms', function() {
+      f.axioms(assert, a, b, c)
+    });
+  })
+});
