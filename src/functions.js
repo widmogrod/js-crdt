d519d66d78100126f308b7f0f5943c1808bcd5e5
widmogrod <widmogrod@gmail.com>
2016-02-08 00:15:22 +0100

    add comments to axioms func

--- src/functions.js
+++ src/functions.js
@@ -12,16 +12,19 @@ function equal(a, b) {
 
 exports.axioms = axioms;
 function axioms(assert, a, b, c) {
+  // commutative   a + c = c + a                i.e: 1 + 2 = 2 + 1
   assert(
     equal(merge(a, b), merge(b, a)),
     'is not commutative'
   );
 
+  // associative   a + (b + c) = (a + b) + c    i.e: 1 + (2 + 3) = (1 + 2) + 3
   assert(
     equal(merge(a, merge(b, c)), merge(merge(a, b), c)),
     'is not associative'
   );
 
+  // idempotent    f(f(a)) = f(a)               i.e: ||a|| = |a|
   assert(
     equal(merge(a, a), a),
     'is idempotent?'
