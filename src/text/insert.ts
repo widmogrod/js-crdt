c622de77975d46e0ff72197b6f28323dae968f34
widmogrod <widmogrod@gmail.com>
2017-08-15 21:03:07 +0200

    apply new formating

--- src/text/insert.ts
+++ src/text/insert.ts
@@ -1,15 +1,15 @@
 import { ensureArrayLength } from '../utils';
 
 export class Insert {
-    constructor(public at: number, public value: string) {
-        this.at = at < 0 ? 0 : at;
-        this.value = String(value);
-    }
+  constructor(public at: number, public value: string) {
+    this.at = at < 0 ? 0 : at;
+    this.value = String(value);
+  }
 
-    apply(data) {
-        let copy = data.slice(0);
-        copy = ensureArrayLength(copy, this.at);
-        copy.splice(this.at, 0, ...this.value.split(''));
-        return copy;
-    }
+  apply(data) {
+    let copy = data.slice(0);
+    copy = ensureArrayLength(copy, this.at);
+    copy.splice(this.at, 0, ...this.value.split(''));
+    return copy;
+  }
 }
