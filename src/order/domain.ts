b976280edf00cc7a124d36b5f008e2e56000c682
widmogrod <widmogrod@gmail.com>
2017-08-15 21:04:22 +0200

    apply new formating

--- src/order/domain.ts
+++ src/order/domain.ts
@@ -1,9 +1,9 @@
 import {Comparer, Merger, Equaler} from '../functions'
 
 export interface Orderer<T> extends
-    Comparer<T>,
-    Merger<T>,
-    Equaler<T>
-{
+  Comparer<T>,
+  Merger<T>,
+  Equaler<T>
+  {
     next(): T
-}
+  }
--- src/order/timestamp.ts
+++ src/order/timestamp.ts
@@ -4,30 +4,30 @@ export type Bucket = string
 export type Time = number
 
 export class Timestamp implements Orderer<Timestamp> {
-    bucket: Bucket
-    time: Time
-    constructor(bucket, time) {
-        this.bucket = bucket;
-        this.time = time;
-    }
-    
-    next() {
-        return new Timestamp(this.bucket, this.time + 1);
+  bucket: Bucket
+  time: Time
+  constructor(bucket, time) {
+    this.bucket = bucket;
+    this.time = time;
+  }
+
+  next() {
+    return new Timestamp(this.bucket, this.time + 1);
+  }
+
+  compare(b: Timestamp): number {
+    if (this.bucket === b.bucket) {
+      return this.time - b.time;
     }
 
-    compare(b: Timestamp): number {
-        if (this.bucket === b.bucket) {
-            return this.time - b.time;
-        }
+    return this.bucket < b.bucket ? -1 : 1;
+  }
 
-        return this.bucket < b.bucket ? -1 : 1;
-    }
-    
-    merge(b: Timestamp): Timestamp {
-        return this;
-    }
-    
-    equal(b: Timestamp): boolean {
-        return false;
-    }
+  merge(b: Timestamp): Timestamp {
+    return this;
+  }
+
+  equal(b: Timestamp): boolean {
+    return false;
+  }
 }
