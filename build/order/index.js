79531e8c9d89656bf78116a881be603bd6b5e177
widmogrod <widmogrod@gmail.com>
2017-08-27 19:32:26 +0200

    nicer import

--- build/order/index.js
+++ build/order/index.js
@@ -4,5 +4,4 @@ function __export(m) {
 }
 Object.defineProperty(exports, "__esModule", { value: true });
 __export(require("./vector-clock"));
-__export(require("./timestamp"));
 //# sourceMappingURL=index.js.map
--- src/order/index.ts
+++ src/order/index.ts
@@ -1,2 +1,1 @@
 export * from './vector-clock';
-export * from './timestamp';
