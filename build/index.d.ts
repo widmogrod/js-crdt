93619ae7e2d8b878fdc7241a3951bf5a3c4c820e
widmogrod <widmogrod@gmail.com>
2017-09-02 22:25:44 +0200

    build & dist

--- build/index.d.ts
+++ build/index.d.ts
@@ -4,4 +4,12 @@ import * as utils from './utils';
 import * as order from './order';
 import * as text from './text';
 import * as structures from './structures';
-export { text, order, utils, functions, increment, structures };
+declare const _default: {
+    text: typeof text;
+    order: typeof order;
+    utils: typeof utils;
+    functions: typeof functions;
+    increment: typeof increment;
+    structures: typeof structures;
+};
+export default _default;
